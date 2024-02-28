d97495fdcdbde566e9c84e32be187ffb9e9f27a2
abiasv <vaziri.as@gmail.com>
2024-02-28 17:55:57 -0500

    test

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -7,90 +7,90 @@ import IMG5 from "../../assets/portfolio5.png";
 import IMG6 from "../../assets/portfolio6.jpg";
 
 const data = [
-  {
-    id: 1,
-    image: IMG1,
-    title: "Crypto Currency Dashboard & Financial Visualization",
-    github: "https://github.com/abiasV/TASKIFY-Typescript",
-    demo: "http://dribbble.com/shots/16673715-Crypto-currency-dashboards-and-financial-data-visualization",
-  },
-  {
-    id: 2,
-    image: IMG2,
-    title: "Charts templates & infographics in Figma",
-    github: "https://github.com",
-    demo: "http://dribbble.com/shots/16673715-Crypto-currency-dashboards-and-financial-data-visualization",
-  },
-  {
-    id: 3,
-    image: IMG3,
-    title: "Figma dashboard UI kit for data design web apps",
-    github: "https://github.com",
-    demo: "http://dribbble.com/shots/16673715-Crypto-currency-dashboards-and-financial-data-visualization",
-  },
-  {
-    id: 4,
-    image: IMG4,
-    title: "Maintaining tasks and tracking progress",
-    github: "https://github.com",
-    demo: "http://dribbble.com/shots/16673715-Crypto-currency-dashboards-and-financial-data-visualization",
-  },
-  {
-    id: 5,
-    image: IMG5,
-    title: "Charts templates & infographics in Figma",
-    github: "https://github.com",
-    demo: "http://dribbble.com/shots/16673715-Crypto-currency-dashboards-and-financial-data-visualization",
-  },
-  {
-    id: 6,
-    image: IMG6,
-    title: "Charts templates & infographics in Figma",
-    github: "https://github.com",
-    demo: "http://dribbble.com/shots/16673715-Crypto-currency-dashboards-and-financial-data-visualization",
-  },
+	{
+		id: 1,
+		image: IMG1,
+		title: "Assignment Tracking Application in ASP.net MVC, C#, JavaScript",
+		github: "https://github.com/abiasV/My-Passion-Project.git",
+		demo: "https://github.com/abiasV/My-Passion-Project.git",
+	},
+	{
+		id: 2,
+		image: IMG2,
+		title: "Travel Website CRUD APPlication in NodeJS, MongoDB, and Express",
+		github: "https://github.com/abiasV/Node.js-Express-app.git",
+		demo: "https://github.com/abiasV/Node.js-Express-app.git",
+	},
+	{
+		id: 3,
+		image: IMG3,
+		title: "The Rick and Morty API in ReactJs",
+		github: "https://github.com/abiasV/Rick-and-Morty.git",
+		demo: "https://github.com/abiasV/Rick-and-Morty.git",
+	},
+	{
+		id: 4,
+		image: IMG4,
+		title: "Egg-Cook Timer in JavaScript, CSS, HTML",
+		github: "https://github.com/abiasV/Cook-Timer.git",
+		demo: "https://github.com/abiasV/Cook-Timer.git",
+	},
+	{
+		id: 5,
+		image: IMG5,
+		title: "Taskify App in TypeScript",
+		github: "https://github.com/abiasV/TASKIFY-Typescript.git",
+		demo: "https://github.com/abiasV/TASKIFY-Typescript.git",
+	},
+	{
+		id: 6,
+		image: IMG6,
+		title: "Recreating and Redesigning(UX Design) an E-Commerce Website in Figma",
+		figma: "https://www.figma.com/file/n0Hu66Q7cnjjBHVcRFHAhd/IXD-Assignment-Tee-On.com-MidFiWireframe?type=design&node-id=2-2&mode=design&t=MJq8Pkvrnfk2Zzz7-0",
+		demo: "https://www.figma.com/file/n0Hu66Q7cnjjBHVcRFHAhd/IXD-Assignment-Tee-On.com-MidFiWireframe?type=design&node-id=2-2&mode=design&t=MJq8Pkvrnfk2Zzz7-0",
+	},
 ];
 
 const Portfolio = () => {
-  return (
-    <section id="portfolio">
-      <h5>My Recent Work</h5>
-      <h2>Portfolio</h2>
+	return (
+		<section id="portfolio">
+			<h5>My Recent Work</h5>
+			<h2>Portfolio</h2>
 
-      <div className="container portfolio__container">
-        {data.map(({ id, image, title, github, demo }) => {
-          return (
-            <article key={id} className="portfolio__item">
-              <div className="portfolio__item-image">
-                <img src={image} alt={title} />
-              </div>
-              <h3>{title}</h3>
-              <div className="portfolio__item-cta">
-                <a
-                  href={github}
-                  className="btn"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  Github
-                </a>
-                <a
-                  href={demo}
-                  className="btn btn-primary"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  Live Demo
-                </a>
-              </div>
-            </article>
-          );
-        })}
+			<div className="container portfolio__container">
+				{data.map(({ id, image, title, github, demo }) => {
+					return (
+						<article key={id} className="portfolio__item">
+							<div className="portfolio__item-image">
+								<img src={image} alt={title} />
+							</div>
+							<h3>{title}</h3>
+							<div className="portfolio__item-cta">
+								<a
+									href={github}
+									className="btn"
+									target="_blank"
+									rel="noreferrer"
+								>
+									Github
+								</a>
+								<a
+									href={demo}
+									className="btn btn-primary"
+									target="_blank"
+									rel="noreferrer"
+								>
+									Live Demo
+								</a>
+							</div>
+						</article>
+					);
+				})}
 
-        {/* END OF IMAGE1 */}
-      </div>
-    </section>
-  );
+				{/* END OF IMAGE1 */}
+			</div>
+		</section>
+	);
 };
 
 export default Portfolio;
